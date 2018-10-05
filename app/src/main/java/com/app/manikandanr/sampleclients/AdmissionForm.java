3a22a005fdef713abf9f5cf88f5ed09eb46d81d7
manikandan R <manikandanr@eaglesoftware.com>
2018-10-05 19:08:20 +0530

    Initial commit

---
+++ app/src/main/java/com/app/manikandanr/sampleclients/AdmissionForm.java
@@ -0,0 +1,13 @@
+package com.app.manikandanr.sampleclients;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AdmissionForm extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_admission_form);
+    }
+}
---
+++ app/src/main/java/com/app/manikandanr/sampleclients/MenuActivity.java
@@ -0,0 +1,82 @@
+package com.app.manikandanr.sampleclients;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MenuActivity extends AppCompatActivity {
+
+    private Button bAdmission,bMarketing,bAttendance,bRevenue,bStore;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu);
+
+        init();
+    }
+    private void init()
+    {
+        bAdmission  = findViewById(R.id.btn_admission);
+        bMarketing = findViewById(R.id.btn_marketing);
+        bAttendance = findViewById(R.id.btn_attendance);
+        bRevenue = findViewById(R.id.btn_revenue);
+        bStore = findViewById(R.id.btn_store);
+
+        bAdmission.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent in = new Intent(MenuActivity.this,AdmissionForm.class);
+                startActivity(in);
+
+            }
+        });
+
+        bMarketing.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent in = new Intent(MenuActivity.this,AdmissionForm.class);
+                startActivity(in);
+
+            }
+        });
+        bAttendance.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent in = new Intent(MenuActivity.this,AdmissionForm.class);
+                startActivity(in);
+            }
+        });
+
+    }
+
+//    public void admissionClick(View v)
+//    {
+//
+//    }
+//    public void marketingClick(View v)
+//    {
+//
+//    }
+//    public void admissionClick(View v)
+//    {
+//
+//    }
+//    public void admissionClick(View v)
+//    {
+//
+//    }
+//    public void admissionClick(View v)
+//    {
+//
+//    }
+
+
+    @Override
+    public void onBackPressed() {
+        finish();
+    }
+}
