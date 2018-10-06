84c99931759d61db752e0aa68e3b5dc83b18ab8b
manikandanr <developer.manikandan2495@gmail.com>
2018-10-06 13:09:35 +0530

    payment-design added1

--- app/src/main/java/com/app/manikandanr/sampleclients/AdmissionForm.java
+++ app/src/main/java/com/app/manikandanr/sampleclients/AdmissionForm.java
@@ -1,7 +1,9 @@
 package com.app.manikandanr.sampleclients;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class AdmissionForm extends AppCompatActivity {
 
@@ -9,5 +11,12 @@ public class AdmissionForm extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admission_form);
+    }
+
+    public void onNextClick(View view)
+    {
+        Intent in = new Intent(AdmissionForm.this,PaymentStatus.class);
+        startActivity(in);
+        finish();
     }
 }
--- app/src/main/java/com/app/manikandanr/sampleclients/MenuActivity.java
+++ app/src/main/java/com/app/manikandanr/sampleclients/MenuActivity.java
@@ -5,6 +5,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
+
+import com.awesomedialog.blennersilva.awesomedialoglibrary.AwesomeInfoDialog;
 
 public class MenuActivity extends AppCompatActivity {
 
@@ -14,9 +17,20 @@ public class MenuActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
         init();
+        //Lib- Lists:
+        //https://android-arsenal.com/details/1/6109
+        //https://android-arsenal.com/details/1/6929
     }
     private void init()
     {
+
+        new AwesomeInfoDialog(this)
+                .setTitle(R.string.app_name)
+                .setMessage(R.string.app_name)
+                .setColoredCircle(R.color.colorPrimary)
+                .setDialogIconAndColor(R.drawable.ic_dialog_info, R.color.white)
+                .setCancelable(true)
+                .show();
 
         bAdmission  = findViewById(R.id.btn_admission);
         bMarketing = findViewById(R.id.btn_marketing);
@@ -34,15 +48,19 @@ public class MenuActivity extends AppCompatActivity {
         bMarketing.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent in = new Intent(MenuActivity.this,AdmissionForm.class);
-                startActivity(in);
+//                Intent in = new Intent(MenuActivity.this,AdmissionForm.class);
+//                startActivity(in);
+                Toast.makeText(MenuActivity.this,"Coming Soon...",Toast.LENGTH_LONG
+                ).show();
             }
         });
         bAttendance.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent in = new Intent(MenuActivity.this,AdmissionForm.class);
-                startActivity(in);
+//                Intent in = new Intent(MenuActivity.this,AdmissionForm.class);
+//                startActivity(in);
+                Toast.makeText(MenuActivity.this,"Coming Soon...",Toast.LENGTH_LONG
+                ).show();
             }
         });
 
