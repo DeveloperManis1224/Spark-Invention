522ec6b82cc24bbf82cebb7f75d65e8726eae77f
ManisMani <developer.manikandan2495@gmail.com>
2019-02-14 23:00:02 +0530

    Check Commits1

--- app/src/main/java/com/app/manikandanr/sampleclients/AddItems.java
+++ app/src/main/java/com/app/manikandanr/sampleclients/AddItems.java
@@ -34,8 +34,6 @@ public class AddItems extends AppCompatActivity {
 
 
 
-
-
     private AutoCompleteTextView txtCountryName;
     ArrayList<String> countryList = new ArrayList<>();
     ArrayList<String> countryIdList = new ArrayList<>();
